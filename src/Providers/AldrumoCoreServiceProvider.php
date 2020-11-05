3aa14bcba821800e70e342b42212c491a139e510
Mike Barlow <mike@mikebarlow.co.uk>
2020-11-05 22:15:37 +0000

    added fortify / jetstream configs

--- src/Providers/AldrumoCoreServiceProvider.php
+++ src/Providers/AldrumoCoreServiceProvider.php
@@ -6,5 +6,24 @@ use Illuminate\Support\ServiceProvider;
 
 class AldrumoCoreServiceProvider extends ServiceProvider
 {
-    //
+    public function register()
+    {
+        $this->mergeConfigFrom(
+            __DIR__ . '/../../config/fortify.php',
+            'fortify'
+        );
+
+        $this->mergeConfigFrom(
+            __DIR__ . '/../../config/jetstream.php',
+            'jetstream'
+        );
+    }
+
+    public function boot()
+    {
+        $this->publishes([
+            __DIR__ . '/../../config/fortify.php'   => config_path('fortify.php'),
+            __DIR__ . '/../../config/jetstream.php' => config_path('jetstream.php'),
+        ]);
+    }
 }
