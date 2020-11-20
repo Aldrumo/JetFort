ac394fd573fcbd73b43dc9404edef3ea2eb3b71d
Mike Barlow <mike@mikebarlow.co.uk>
2020-11-20 12:29:02 +0000

    tests

---
+++ tests/Providers/AldrumoCoreServerProviderTest.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Aldrumo\Core\Tests\Providers;
+
+class AldrumoCoreServerProviderTest extends TestCase
+{
+    /** @test */
+    public function test()
+    {
+        $this->assertTrue(true);
+    }
+}
---
+++ tests/TestCase.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Aldrumo\Core\Tests;
+
+use Aldrumo\Core\Providers\AldrumoCoreServiceProvider;
+use Orchestra\Testbench\TestCase as BaseTestCase;
+
+class TestCase extends BaseTestCase
+{
+    protected function getPackageProviders($app)
+    {
+        return [
+            AldrumoCoreServiceProvider::class,
+        ];
+    }
+}
