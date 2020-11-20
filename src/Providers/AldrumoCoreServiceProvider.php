d12cc14749da18edaae82df31c979c054e9a2c16
Mike Barlow <mike@mikebarlow.co.uk>
2020-11-20 17:02:10 +0000

    routes

--- src/Providers/AldrumoCoreServiceProvider.php
+++ src/Providers/AldrumoCoreServiceProvider.php
@@ -3,17 +3,66 @@
 namespace Aldrumo\Core\Providers;
 
 use Aldrumo\ThemeManager\ThemeManager;
+use Illuminate\Support\Facades\Route;
 use Illuminate\Support\ServiceProvider;
 
 class AldrumoCoreServiceProvider extends ServiceProvider
 {
     public function register()
     {
-        $this->app->booting(function ($app) {
-            $app->register(FortifyServiceProvider::class);
-            $app->register(JetstreamServiceProvider::class);
-        });
+        $this->registerConfigs();
+        $this->registerServerProviders();
+    }
 
+    public function boot()
+    {
+        resolve(ThemeManager::class)->activeTheme('DefaultTheme');
+
+        $this->bootMigrations();
+        $this->bootPublishes();
+        $this->bootRoutes();
+    }
+
+    protected function bootMigrations()
+    {
+        $this->loadMigrationsFrom(__DIR__ . '/../../database/migrations');
+    }
+
+    protected function bootPublishes()
+    {
+        $this->publishes([
+            __DIR__ . '/../../config/sanctum.php'   => config_path('sanctum.php'),
+            __DIR__ . '/../../config/fortify.php'   => config_path('fortify.php'),
+            __DIR__ . '/../../config/jetstream.php' => config_path('jetstream.php'),
+        ]);
+    }
+
+    protected function bootRoutes()
+    {
+        Route::middleware('web')
+            ->group(
+                function () {
+                    $this->loadRoutesFrom(__DIR__ . '/../../routes/web.php');
+                }
+            );
+
+        Route::middleware('web')
+            ->group(
+                function () {
+                    //RouteLoader::generateRoutes();
+                }
+            );
+
+        Route::middleware('api')
+            ->group(
+                function () {
+                    $this->loadRoutesFrom(__DIR__ . '/../../routes/api.php');
+                }
+            );
+    }
+
+    protected function registerConfigs()
+    {
         $this->mergeConfigFrom(
             __DIR__ . '/../../config/sanctum.php',
             'sanctum'
@@ -30,16 +79,11 @@ class AldrumoCoreServiceProvider extends ServiceProvider
         );
     }
 
-    public function boot()
+    protected function registerServerProviders()
     {
-        resolve(ThemeManager::class)->activeTheme('DefaultTheme');
-
-        $this->loadMigrationsFrom(__DIR__ . '/../../database/migrations');
-
-        $this->publishes([
-            __DIR__ . '/../../config/sanctum.php'   => config_path('sanctum.php'),
-            __DIR__ . '/../../config/fortify.php'   => config_path('fortify.php'),
-            __DIR__ . '/../../config/jetstream.php' => config_path('jetstream.php'),
-        ]);
+        $this->app->booting(function ($app) {
+            $app->register(FortifyServiceProvider::class);
+            $app->register(JetstreamServiceProvider::class);
+        });
     }
 }
