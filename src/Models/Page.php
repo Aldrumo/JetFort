8fb88cd020721113055f76f5da89b3bd0224c2c2
Mike Barlow <mike@mikebarlow.co.uk>
2020-12-01 09:14:06 +0000

    added sluggable to model

--- src/Models/Page.php
+++ src/Models/Page.php
@@ -3,13 +3,28 @@
 namespace Aldrumo\Core\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Spatie\Sluggable\HasSlug;
+use Spatie\Sluggable\SlugOptions;
 
 class Page extends Model
 {
+    use HasSlug;
+
     protected $fillable = [
         'title',
         'slug',
         'template',
         'is_active',
     ];
+
+    /**
+     * Get the options for generating the slug.
+     */
+    public function getSlugOptions() : SlugOptions
+    {
+        return SlugOptions::create()
+            ->generateSlugsFrom('title')
+            ->saveSlugsTo('slug')
+            ->slugsShouldBeNoLongerThan(250);
+    }
 }
