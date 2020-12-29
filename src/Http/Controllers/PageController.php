4124f32f6efb8918ff5ebf429f5834a479a63da3
Mike Barlow <mike@mikebarlow.co.uk>
2020-12-29 20:26:54 +0000

    wip

--- src/Http/Controllers/PageController.php
+++ src/Http/Controllers/PageController.php
@@ -2,10 +2,12 @@
 
 namespace Aldrumo\Core\Http\Controllers;
 
+use Illuminate\Http\Request;
+
 class PageController
 {
-    public function __invoke()
+    public function __invoke(Request $request)
     {
-        //
+        dd($request);
     }
 }
