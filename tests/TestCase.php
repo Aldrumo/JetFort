65f2c59e6fbef5116266dbcc1f38d2ef5893bc8c
Mike Barlow <mike@mikebarlow.co.uk>
2020-12-29 20:38:17 +0000

    wip

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -3,8 +3,11 @@
 namespace Aldrumo\Core\Tests;
 
 use Aldrumo\Core\Providers\AldrumoCoreServiceProvider;
+use Aldrumo\Core\Tests\Fixtures\EmptyRouteLoader;
 use Aldrumo\Core\Tests\Fixtures\TestClasses\AnotherThemeServiceProvider;
 use Aldrumo\Core\Tests\Fixtures\TestClasses\DefaultThemeServiceProvider;
+use Aldrumo\RouteLoader\Contracts\RouteLoader;
+use Aldrumo\RouteLoader\RouteLoaderServiceProvider;
 use Livewire\LivewireServiceProvider;
 use Orchestra\Testbench\TestCase as BaseTestCase;
 
@@ -17,6 +20,27 @@ class TestCase extends BaseTestCase
             AnotherThemeServiceProvider::class,
             LivewireServiceProvider::class,
             AldrumoCoreServiceProvider::class,
+            RouteLoaderServiceProvider::class,
         ];
     }
+
+    /**
+     * Define environment setup.
+     *
+     * @param  \Illuminate\Foundation\Application  $app
+     * @return void
+     */
+    protected function getEnvironmentSetUp($app)
+    {
+        $app['config']->set(
+            'routeloader.controller',
+            'PageController@load'
+        );
+
+        $app->bind(
+            RouteLoader::class,
+            EmptyRouteLoader::class
+        );
+    }
+
 }
