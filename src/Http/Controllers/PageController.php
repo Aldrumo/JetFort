e022082ee7c753ea7e9b1237ab5fe10b0fbb5dd3
Mike Barlow <mike@mikebarlow.co.uk>
2021-01-05 07:37:17 +0000

    wip

--- src/Http/Controllers/PageController.php
+++ src/Http/Controllers/PageController.php
@@ -2,12 +2,39 @@
 
 namespace Aldrumo\Core\Http\Controllers;
 
+use Aldrumo\Core\Models\Page;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\View;
 
 class PageController
 {
     public function __invoke(Request $request)
     {
-        dd($request);
+        $id = $this->findPageId($request);
+
+        $page = Page::where('id', $id)
+            ->where('is_active', 1)
+            ->first();
+
+        if ($page === null) {
+            abort(404);
+        }
+
+        View::share([
+            'inEditor' => false,
+            'contentBlocks' => $page->blocks,
+        ]);
+
+        return view(
+            $page->template,
+            [
+                'page' => $page,
+            ]
+        );
+    }
+
+    protected function findPageId(Request $request) : int
+    {
+        return str_replace('route-', '', $request->route()->getName());
     }
 }
