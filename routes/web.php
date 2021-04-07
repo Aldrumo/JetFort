f8ef3b2bfc0e5a03c16717efffe44a127d31d9d6
Mike Barlow <mike@mikebarlow.co.uk>
2021-04-07 09:03:39 +0100

    merging in the default page package

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,4 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::view('/', 'AldrumoDefaults::default-page');
-
-// @todo Route loader stuff here
+Route::view('/', 'AldrumoCore::default-page');
